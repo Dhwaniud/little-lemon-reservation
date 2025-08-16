7f73edeb49c9d6e080652ceb2ff37c0e353b4feb
Dhwani Desai <dhwanid11@gmail.com>
2025-08-16 22:38:49 +0100

    specials section styles fix

--- src/components/Specials/Card.js
+++ src/components/Specials/Card.js
@@ -1,14 +1,21 @@
 import Scooter from "../../assets/scooter.svg";
+import DishIcon from "../../assets/dish-icon.svg";
 
 function Card({ name, price, description, imgSrc, altText }) {
     return (
         <article className="card">
             <img src={imgSrc} alt={altText} className="card-image" />
             <div className="card-body">
-                <h4 className="card-title">{name}</h4>
+                <h4 className="card-title">
+                    <div className="text-left">
+                        <span>{name}</span>
+                        <img src={DishIcon} alt="Dish icon" />
+                    </div>
+                    <div className="text-right">{`$${price.toFixed(2)}`}</div>
+                </h4>
                 <p className="card-description">{description}</p>
                 <h6 className="card-action">
-                    <span>Order a delivery</span>
+                    <a href="">Order a delivery</a>
                     <img src={Scooter} alt="Scooter" />
                 </h6>
             </div>
