749ceca78ab508cf5571b31590ec3a920cbd5105
Dhwani Desai <dhwanid11@gmail.com>
2025-08-16 17:51:12 +0100

    add sematic html tags

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import './App.css';
+import "./App.css";
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <>
-      <Nav />
-      <Header/>
-      <Main/>
-      <Footer/>
-      Homepage
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Nav />
+            <Main />
+            <Footer />
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import Logo from "../assets/FooterLogo.png";
+
+function Footer() {
+    return (
+        <footer>
+            <img src={Logo} alt="Footer logo" />
+        </footer>
+    );
+}
+
+export default Footer;
