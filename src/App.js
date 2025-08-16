1d11dc207fa25f781076ea68fc58dc2ee338bcc8
Dhwani Desai <dhwanid11@gmail.com>
2025-08-16 13:44:24 +0100

    Created components for home page and added meta tags to index.html

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Nav />
+      <Header/>
+      <Main/>
+      <Footer/>
       Homepage
-    </div>
+    </>
   );
 }
 
